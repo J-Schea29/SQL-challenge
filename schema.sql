91bdda4ae9a52faab48ea1ab3c3403ddeb76831c
Jon Scheaffer <84929443+J-Schea29@users.noreply.github.com>
2021-08-23 16:23:03 -0700

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE titles (
     title_id VARCHAR NOT NULL,
     title VARCHAR NOT NULL,
     PRIMARY KEY(title_id)
-):
+);
 
 CREATE TABLE empLoyees (
     emp_no INT NOT NULL,
@@ -12,15 +12,15 @@ CREATE TABLE empLoyees (
     last_name VARCHAR(20) NOT NULL,
     sex VARCHAR(1) NOT NULL,
     hire_date DATE NOT NULL,
-    PRIMARY KEY(emp_no)
+    PRIMARY KEY(emp_no),
     FOREIGN KEY (title_id) REFERENCES titles (title_id)
-):
+);
 
 CREATE TABLE departments (
     dept_no VARCHAR(4) NOT NULL,
     dept_name VARCHAR NOT NULL,
     PRIMARY KEY(dept_no)
-):
+);
 
 CREATE TABLE dept_manager (
     dept_no VARCHAR(4) NOT NULL,
@@ -28,19 +28,40 @@ CREATE TABLE dept_manager (
     FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
     FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
     PRIMARY KEY(emp_no, dept_no)
-):
+);
 
 CREATE TABLE dept_emp (
-    dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
+    dept_no VARCHAR(4) NOT NULL,
     FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
     FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
     PRIMARY KEY(emp_no, dept_no)
-):
+);
 
 CREATE TABLE salaries (
     emp_no INT NOT NULL,
     salary INT NOT NULL,
     FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
     PRIMARY KEY(emp_no)
-):
+);
+
+
+SELECT * FROM titles;
+
+SELECT * FROM employees;
+
+SELECT * FROM departments;
+
+SELECT * FROM dept_manager;
+
+SELECT * FROM dept_emp;
+
+SELECT * FROM salaries;
+
+SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.emp_no, s.salary
+FROM employees e
+LEFT JOIN salaries s
+ON e.emp_no = s.emp_no
+
+
+
