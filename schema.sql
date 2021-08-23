0595130063ddc7b24c2b1720ff8c3af3b85255e8
Jon Scheaffer <84929443+J-Schea29@users.noreply.github.com>
2021-08-23 15:00:15 -0700

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,29 +1,46 @@
-CREATE TABLE departments (
-    dept_no VARCHAR(4) NOT NULL
-    dept_name VARCHAR NOT NULL
-):
-
-CREATE TABLE dept_manager (
-    dept_no VARCHAR(4) NOT NULL
-    emp_no INT NOT NULL
+CREATE TABLE titles (
+    title_id VARCHAR NOT NULL,
+    title VARCHAR NOT NULL,
+    PRIMARY KEY(title_id)
 ):
 
 CREATE TABLE empLoyees (
-    emp_no INT NOT NULL
-    emp_title_id VARCHAR NOT NULL
-    birth_date DATE NOT NULL
-    first_name VARCHAR(20) NOT NULL
-    last_name VARCHAR(20) NOT NULL
-    sex VARCHAR(1) NOT NULL
-    hire_date DATE NOT NULL
+    emp_no INT NOT NULL,
+    title_id VARCHAR NOT NULL,
+    birth_date DATE NOT NULL,
+    first_name VARCHAR(20) NOT NULL,
+    last_name VARCHAR(20) NOT NULL,
+    sex VARCHAR(1) NOT NULL,
+    hire_date DATE NOT NULL,
+    PRIMARY KEY(emp_no)
+    FOREIGN KEY (title_id) REFERENCES titles (title_id)
+):
+
+CREATE TABLE departments (
+    dept_no VARCHAR(4) NOT NULL,
+    dept_name VARCHAR NOT NULL,
+    PRIMARY KEY(dept_no)
+):
+
+CREATE TABLE dept_manager (
+    dept_no VARCHAR(4) NOT NULL,
+    emp_no INT NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+    PRIMARY KEY(emp_no, dept_no)
+):
+
+CREATE TABLE dept_emp (
+    dept_no VARCHAR(4) NOT NULL,
+    emp_no INT NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+    PRIMARY KEY(emp_no, dept_no)
 ):
 
 CREATE TABLE salaries (
-    emp_no INT NOT NULL
-    salary INT NOT NULL
-):
-
-CREATE TABLE titles (
-    title_id VARCHAR NOT NULL
-    title VARCHAR NOT NULL
+    emp_no INT NOT NULL,
+    salary INT NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    PRIMARY KEY(emp_no)
 ):
