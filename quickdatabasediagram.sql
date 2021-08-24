73091e2ca2a7b09f703cc0adc03756e3b48982a1
Jon Scheaffer <84929443+J-Schea29@users.noreply.github.com>
2021-08-24 08:08:29 -0700

    Update quickdatabasediagram.sql

--- quickdatabasediagram.sql
+++ quickdatabasediagram.sql
@@ -1,35 +1,34 @@
+titles 
+-
+title_id VARCHAR PK
+title VARCHAR 
+
 empLoyees
 -
 emp_no INT PK
-title_id VARCHAR 
+title_id VARCHAR FK >- titles.title_id
 birth_date DATE 
 first_name VARCHAR(20) 
 last_name VARCHAR(20) 
 sex VARCHAR(1) 
 hire_date DATE 
 
-salaries 
--
-emp_no INT PK FK >- empLoyees.emp_no
-salary INT 
-
 departments 
 -
 dept_no VARCHAR(4) PK
 dept_name VARCHAR 
-
-dept_emp
--
-emp_no PK FK >- empLoyees.emp_no
-dept_no PK FK >- departments.dept_no
 
 dept_manager 
 -
 dept_no VARCHAR(4) PK FK >- departments.dept_no
 emp_no INT FK >- empLoyees.emp_no
 
-
-titles 
+dept_emp
 -
-title_id VARCHAR PK FK >- empLoyees.title_id
-title VARCHAR 
+emp_no PK FK >- empLoyees.emp_no
+dept_no PK FK >- departments.dept_no
+
+salaries 
+-
+emp_no INT PK FK >- empLoyees.emp_no
+salary INT 
